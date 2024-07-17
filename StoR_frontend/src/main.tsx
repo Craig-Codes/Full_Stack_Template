e5768bf0f2894b0f24af80ea1e362cb3b34aeb07
Keiran Hazell <Keiran.hazell@gmail.com>
2024-07-17 14:30:32 +0100

    added material UI theme

--- StoR_frontend/src/main.tsx
+++ StoR_frontend/src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from '@mui/material'
+import theme from './theme/theme.ts'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App/>
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
 
