8245ce27c0c3c236de0b23c450e3c8671447f6f9
Craig <craig.adam@appivate.mod.uk>
2024-07-19 16:55:42 +0100

    express async workaround

--- StoR_backend/src/index.ts
+++ StoR_backend/src/index.ts
@@ -1,20 +1,43 @@
-// src/index.ts
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+const { Client } = pg;
+const database = new Client({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  database: process.env.POSTGRES_DB,
 });
 
-app.get('/equipment', (req, res) => {
-  res.json({message:'Test'});
+const connectDatabase = async () => {
+  await database.connect();
+  console.log("connected?");
+};
+
+connectDatabase();
+
+app.get("/", async (req, res) => {
+  console.log("Hit home route");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get("/equipment", async (req, res) => {
+  try {
+    const equiment = await database.query("SELECT * FROM equipment;");
+    console.log(equiment.rows);
+    res.status(200).json({ equpment: equiment.rows });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Unable to retrieve equipment - try harder" });
+  }
 });
