e28fe779d8f1000cdca4853cdbe38d90b881da1f
Keiran Hazell <Keiran.hazell@gmail.com>
2024-07-19 15:37:00 +0100

    frontend to backend

--- StoR_frontend/src/pages/home.tsx
+++ StoR_frontend/src/pages/home.tsx
@@ -1,8 +1,28 @@
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { useEffect, useState } from 'react';
+
 
 export const HomePage = () => {
-    console.log('test')
+  const [equipment, setEquipment] = useState(null)
+  const [fecthError, setFecthError] = useState('')
+
+  useEffect( () => {
+    const fetchEquipment = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/equipment");
+      const retrieveEquipment = await response.json();
+      setEquipment(retrieveEquipment)
+      console.log(retrieveEquipment)
+      } catch {
+        setFecthError('Failed to retieve equipment!')
+      }
+    }
+    fetchEquipment()
+  
+  },[])
+
+    
     return (
     <>
     <h2>Hi, I am a Home Page!</h2>
@@ -10,6 +30,7 @@ export const HomePage = () => {
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+      <p>{equipment?.message}</p>
     </Stack>
     </>
     )
