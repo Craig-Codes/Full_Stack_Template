9272a3cf72fa6da192db007746e3dbe8419a7bcf
Keiran Hazell <Keiran.hazell@gmail.com>
2024-07-19 15:37:00 +0100

    frontend to backend

--- StoR_backend/src/index.ts
+++ StoR_backend/src/index.ts
@@ -1,11 +1,18 @@
 // src/index.ts
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
+});
+
+app.get('/equipment', (req, res) => {
+  res.json({message:'Test'});
 });
 
 app.listen(port, () => {
--- StoR_frontend/src/pages/home.tsx
+++ StoR_frontend/src/pages/home.tsx
@@ -1,27 +1,34 @@
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { useEffect, useState } from 'react';
-
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
 
 export const HomePage = () => {
+  const [equipment, setEquipment] = useState(null);
+  const [fetchError, setFetchError] = useState("");
 
-    const [equipment, setEquipment] = useState()
+  useEffect(() => {
+    const fetchEquipment = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/equipment");
+        const retrieveEquipment = await response.json();
+        setEquipment(retrieveEquipment);
+        console.log(retrieveEquipment);
+      } catch {
+        setFetchError("Failed to retieve equipment!");
+      }
+    };
+    fetchEquipment();
+  }, []);
 
-    useEffect(() => {
-      console.log('test')
-    },[])
-
-    
-    return (
+  return (
     <>
-    <h2>Hi, I am a Home Page!</h2>
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+      <h2>Hi, I am a Home Page!</h2>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <p>{equipment?.message}</p>
+      </Stack>
     </>
-    )
-  }
-
-
+  );
+};
